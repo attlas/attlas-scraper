1e1e76e1c129b366531ed53b199c1f26f8ade82e
VladyslavKurmaz <vladislav.kurmaz@gmail.com>
2017-11-26 14:08:17 +0000

    Collect vacancies

--- services/demon/scripts/vacancies/dou.ua/post.php
+++ services/demon/scripts/vacancies/dou.ua/post.php
@@ -6,8 +6,29 @@ $cntx = new \atlas\context(__DIR__);
 if (count($argv) === 2){
   $cntxData = new \atlas\context($argv[1]);
   //
-  $cntxData->storage->putFileContent(array(), 'attrs.json', '1111111111');
-  
+  $f = 'dou.html';
+  $cntx->logger->echoLn("[+] Start processing {$f}");
+  $rowHtml = $cntx->storage->getFileContent(array(), $f);
+  $html = str_get_html($rowHtml);
+  $r = array();
+  if($html){
+    foreach($html->find('.cn-a') as $element){
+      $arr = explode("/", $element->href);
+      array_pop($arr);
+      $id = array_pop($arr);
+      $r[] = (object)array("id" => $id);
+      $cntx->logger->tick();
+    }
+    $html->clear();
+    unset($html);
+    $cntx->logger->echoLn();
+  } else { 
+    $cntx->logger->echoLn("Parse error");
+  }
+  //
+  $cntxData->storage->putFileContent(array(), 'attrs.json', json_encode((object)array('companies' => $r)));
+  $cnt = count($r);
+  $cntx->logger->echoLn("[-] Done, number of companies {$cnt}");
 } else {
   $cntx->logger->echoLn("Invalid parameters count");
 }
@@ -16,28 +37,7 @@ if (count($argv) === 2){
   Extract companies from html page
 */
 /*
-$path = $cntx->storage->constructPath('dou.html');
-$cntx->logger->echoLn("Start processing {$path}");
-$rowHtml = $cntx->storage->getFileContent($path);
-$html = str_get_html($rowHtml);
-$r = array();
-if($html){
-  foreach($html->find('.cn-a') as $element){
-    $arr = explode("/", $element->href);
-    array_pop($arr);
-    $id = array_pop($arr);
-    $r[] = (object)array("id" => $id);
-    $cntx->logger->tick();
-  }
-  $html->clear();
-  unset($html);
-  $cntx->logger->echoLn();
-} else { 
-  $cntx->logger->echoLn("Parse error");
-}
 //print_r($r);
 $cntx->storage->putFileContent($cntx->storage->constructPath('dou.json'), json_encode($r));
-$cnt = count($r);
-$cntx->logger->echoLn("Done, number of companies {$cnt}");
 */
 ?>
