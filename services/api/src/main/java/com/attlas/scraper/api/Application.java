c3a2a43ed03db2ec2965bfc4ec0e2e831d5c62c2
IgorRudenko <fac30ff@gmail.com>
2018-07-11 12:28:00 +0300

    add temporary solution for seting port

--- services/api/src/main/java/com/attlas/scraper/api/Application.java
+++ services/api/src/main/java/com/attlas/scraper/api/Application.java
@@ -5,15 +5,33 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+@ComponentScan("com.attlas.scraper.api.configuration")
 @SpringBootApplication
 public class Application {
+
+  private static class ConfMap {
+
+    protected Map<String, Object> confMap () {
+      Map<String, Object> map = new HashMap<>();
+      String component_param_port = Optional.ofNullable(System.getenv("COMPONENT_PARAM_PORT")).orElse("9999");
+      map.put("server.port", component_param_port);
+      return map;
+    }
+  }
 
   public static void main(String[] args) {
     //PropertiesUtil.initProperties();
     new SpringApplicationBuilder()
         .bannerMode(Banner.Mode.OFF)
         .sources(Application.class)
+        .properties(new ConfMap().confMap())
         .run(args);
+
   }
 }
