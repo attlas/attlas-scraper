287f5d86df9e2f56185adcc35da3092690a6d92e
VladyslavKurmaz <vladislav.kurmaz@gmail.com>
2018-07-20 19:24:52 +0000

    Refactoring env vars reading procedure + logger

--- services/api/src/main/java/com/attlas/scraper/api/configuration/CorsCustomizer.java
+++ services/api/src/main/java/com/attlas/scraper/api/configuration/CorsCustomizer.java
@@ -1,8 +1,7 @@
 package com.attlas.scraper.api.configuration;
 
-import com.attlas.scraper.api.constants.AppConst;
-import com.attlas.scraper.api.utils.EnvironmentVariablesLoader;
-import com.attlas.scraper.api.utils.VarTransfer;
+import com.attlas.scraper.api.ApplicationParameter;
+import com.attlas.scraper.api.Context;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -13,12 +12,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @EnableWebMvc
 public class CorsCustomizer implements WebMvcConfigurer {
 
-  private EnvironmentVariablesLoader loader = EnvironmentVariablesLoader.getInstance();
-  private VarTransfer transfer = VarTransfer.getInstance();
-
   @Override
   public void addCorsMappings(CorsRegistry registry) {
-    registry.addMapping("/" + transfer.varAsString(loader.receiveEnvironmentVariable(AppConst.CORS)));
+    registry.addMapping("/" + Context.getInstance().asString(ApplicationParameter.CORS));
   }
 
 
--- services/api/src/main/java/com/attlas/scraper/api/configuration/ServerPortCustomizer.java
+++ services/api/src/main/java/com/attlas/scraper/api/configuration/ServerPortCustomizer.java
@@ -1,8 +1,9 @@
 package com.attlas.scraper.api.configuration;
 
-import com.attlas.scraper.api.constants.AppConst;
-import com.attlas.scraper.api.utils.EnvironmentVariablesLoader;
-import com.attlas.scraper.api.utils.VarTransfer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import com.attlas.scraper.api.ApplicationParameter;
+import com.attlas.scraper.api.Context;
 
 import org.springframework.boot.web.server.ConfigurableWebServerFactory;
 import org.springframework.boot.web.server.WebServerFactoryCustomizer;
@@ -11,11 +12,11 @@ import org.springframework.stereotype.Component;
 @Component
 public class ServerPortCustomizer implements WebServerFactoryCustomizer<ConfigurableWebServerFactory> {
 
-  private EnvironmentVariablesLoader loader = EnvironmentVariablesLoader.getInstance();
-  private VarTransfer transfer = VarTransfer.getInstance();
+  private static final Logger logger = LoggerFactory.getLogger(ServerPortCustomizer.class);
 
   @Override
   public void customize(ConfigurableWebServerFactory factory) {
-    factory.setPort(transfer.varAsInteger(loader.receiveEnvironmentVariable(AppConst.PORT)));
+    final int port = Context.getInstance().asInt(ApplicationParameter.PORT);
+    factory.setPort(port);
   }
 }
